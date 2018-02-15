c6ead49eb8886433e5d398a41f4ed2047f9941df
malevinson <matt.a.levinson@gmail.com>
2018-02-15 10:39:27 -0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,7 +1,42 @@
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+    end
+end
+
+set -x PATH ~/.local/bin $PATH
+
+
+function g
+   git
+end
+
+function gk
+   gitk
+end
+
+function sudo
+    if test "$argv" = !!
+        eval command sudo $history[1]
+    else
+        command sudo $argv
+    end
+end
+"~/.config/fish/config.fish" 35L, 387C
 function fish_user_key_bindings
     for mode in insert default visual
         bind -M $mode \cf forward-char
     end
+end
+
+set -x PATH ~/.local/bin $PATH
+
+
+function g
+   git
+end
+
+function gk
+   gitk
 end
 
 function sudo
@@ -19,3 +54,95 @@ end
 function cd..
     cd ..
 end
+
+
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+    end
+end
+
+set -x PATH ~/.local/bin $PATH
+
+
+function g
+   git
+end
+
+function gk
+   gitk
+end
+
+function sudo
+    if test "$argv" = !!
+        eval command sudo $history[1]
+    else
+        command sudo $argv
+    end
+end
+
+function ..
+    cd ..
+end
+
+function cd..
+    cd ..
+end
+
+
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
