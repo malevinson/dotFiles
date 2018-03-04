2429fb6dd26b7376698b31f7784e946713917dc6
malevinson <matt.a.levinson@gmail.com>
2018-03-04 12:21:00 -0800

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,65 +1,33 @@
-# name: trout
+function fish_prompt --description 'Write out the prompt'
+	set -l last_status $status
 
-function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
+    # User
+    set_color $fish_color_user
+    echo -n (whoami)
+    set_color normal
 
-function g
- git $argv
-end
+    echo -n '@'
 
-function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-end
+    # Host
+    set_color $fish_color_host
+    echo -n (prompt_hostname)
+    set_color normal
 
-function _file_count
-  ls -1 | wc -l | sed 's/\ //g'
-end
+    echo -n ':'
 
-function fish_prompt
-  set -l last_status $status
-  set -l yellow (set_color yellow)
-  set -l red (set_color red)
-  set -l green (set_color green)
-  set -l magenta (set_color magenta)
-  set -l normal (set_color normal)
+    # PWD
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
 
-  if [ (rbenv version-name) ]
-    set ruby_version (rbenv version-name)
-    set ruby_info "$red($ruby_version)$normal"
-  end
+    __terlar_git_prompt
+    __fish_hg_prompt
+    echo
 
-  if [ (_git_branch_name) ]
-    set -l git_branch $magenta(_git_branch_name)
-    set git_info "$git_branch"
-
-    if [ (_is_git_dirty) ]
-      set git_info "$git_info$yellow*"
+    if not test $last_status -eq 0
+        set_color $fish_color_error
     end
 
-    set git_info "$magenta($git_info$magenta)$normal"
-  end
-
-  if test $last_status = 0
-    set prompt " $green☆$normal "
-  else
-    set prompt " $red☆$normal "
-  end
-
-  echo -n -s $ruby_info $git_info $prompt
+    echo -n '➤ '
+    set_color normal
 end
-
-function fish_right_prompt
-  set -l blue (set_color blue)
-  set -l normal (set_color normal)
-
-  set -l path (pwd)
-  set -l count (_file_count)
-  set -l directory_info "($path ($count))"
-
-  set -l time (date '+%I:%M')
-  set -l time_info "$blue($time)$normal"
-
-  echo -n -s $directory_info $time_info
-end
-"fish_prompt.fish" 64L, 1357C
